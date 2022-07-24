d97d0e1a9635127502cd720ca7ea8e118cbfc44c
jamalChelski <ifista202522035@gmail.com>
2022-07-24 16:59:53 +0700

    menambahkan query select all ke aplikasi

--- controller.js
+++ controller.js
@@ -4,5 +4,19 @@ var response = require("./res");
 var connection = require("./koneksi");
 
 exports.index = function (req, res) {
-  response.ok("aplikasi REST API sudah berjalan");
+  response.ok("aplikasi REST API sudah berjalan", res);
+};
+
+//menampilkan semua data mahasiswa
+exports.tampilSemuaMahasiswa = function (req, res) {
+  connection.query(
+    "SELECT * FROM tbl_mahasiswa",
+    function (error, rows, fields) {
+      if (error) {
+        connection.log(error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
 };
