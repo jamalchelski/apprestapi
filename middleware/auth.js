d60a64f625d21cfd6f50b06b7a019bc66a0983ac
jamalChelski <ifista202522035@gmail.com>
2022-07-24 20:47:56 +0700

    menambahkan controller auth

---
+++ middleware/auth.js
@@ -0,0 +1,45 @@
+var connection = require("../koneksi");
+var mysql = require("mysql");
+var md5 = require("md5");
+var response = require("../res");
+var jwt = require("jsonwebtoken");
+var config = require("../config/secret");
+var ip = require("ip");
+
+//controller untuk registrasi
+
+exports.registrasi = function (req, res) {
+  var post = {
+    username: req.body.username,
+    email: req.body.email,
+    password: md5(req.body.password),
+    role: req.body.role,
+    tgl_daftar: new Date(),
+  };
+
+  var query = "SELECT email FROM ?? WHERE ??";
+  var table = ["tbl_users", "email", post.email];
+
+  query = mysql.format(query, table);
+
+  connection.query(query, function (error, rows) {
+    if (error) {
+      console.log(error);
+    } else {
+      if (rows.length == 0) {
+        var query = "INSERT INTO ?? SET ??";
+        var table = ["tbl_users"];
+        query = mysql.format(query, table);
+        connection.query(query, post, function (error, rows) {
+          if (error) {
+            console.log(error);
+          } else {
+            response.ok("Berhasil menambahkan data user baru", res);
+          }
+        });
+      } else {
+        response.ok("Email Sudah Terdaftar !");
+      }
+    }
+  });
+};
